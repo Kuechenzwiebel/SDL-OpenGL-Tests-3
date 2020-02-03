186fb1205575ea1e76deccedda54fe1f03a72032
Tobias Pflüger <tobiaspflueger@t-online.de>
2020-02-03 16:55:55 +0100

    Added experimental spotlight and made light calculations more efficient

--- SDL-OpenGL-Tests-3/resources/shader/basic.fs
+++ SDL-OpenGL-Tests-3/resources/shader/basic.fs
@@ -14,12 +14,18 @@ uniform vec3 viewPos;
 const float linear = 0.07f;
 const float quadratic = 0.017f;
 
-const vec3 ambientLight = vec3(0.0f);
+const vec3 ambientLight = vec3(0.1f);
 
 
 struct LightSourceData {
     vec3 lightColor;
     vec3 lightPos;
+};
+
+struct DirectionalLightData {
+    vec3  position;
+    vec3  direction;
+    float cutOff;
 };
 
 uniform LightSourceData lights[100];
@@ -31,44 +37,79 @@ uniform sampler2D reflectionMap;
 void main() {
     vec3 result = vec3(0.0f);
     
-    color = vec4(viewPos, 1.0f);
+    
+    vec3 normal = normalize(Normal);
+    vec3 lightDir;
+    float diff;
+    vec3 diffuse;
+    vec3 viewDir = normalize(viewPos - Vertex);
+    vec3 reflectDir;
+    
+    
+    int internalReflection;
+    if(useReflectionMap == 0) {
+        internalReflection = reflection;
+    }
+    else {
+        vec3 refMap = texture(reflectionMap, UV).rgb;
+        internalReflection = int((refMap.r + refMap.g + refMap.b) / 3.0f * 128);
+    }
+    
+    vec3 specular;
+    
+    
+    float dist;
+    float attenuation;
     
     for(int i = 0; i < validObjects; i++) {
-        vec3 normal = normalize(Normal);
+        lightDir = normalize(lights[i].lightPos - Vertex);
+        diff = max(dot(normal, lightDir), 0.0f);
+        diffuse = diff * lights[i].lightColor;
         
-        vec3 lightDir = normalize(lights[i].lightPos - Vertex);
-        vec3 diffuse = max(dot(normal, lightDir), 0.0f) * lights[i].lightColor;
+        reflectDir = reflect(-lightDir, normal);
         
-        vec3 viewDir = normalize(viewPos - Vertex);
-        vec3 reflectDir = reflect(-lightDir, normal);
         
-        int ref;
-        if(useReflectionMap == 0) {
-            ref = reflection;
-        }
-        else {
-            vec3 refMap = texture(reflectionMap, UV).rgb;
-            ref = int((refMap.r + refMap.g + refMap.b) / 3.0f * 128);
-        }
-        
-        vec3 specular;
-        
-        if(ref == 0) {
+        if(internalReflection == 0) {
             specular = vec3(0.0f);
         }
         else {
-            specular = 0.5f * pow(max(dot(viewDir, reflectDir), 0.0f), ref) * lights[i].lightColor;
+            specular = 0.5f * pow(max(dot(viewDir, reflectDir), 0.0f), internalReflection) * lights[i].lightColor;
         }
-    
-    
-        float dist = length(lights[i].lightPos - Vertex);
-        float attenuation = 1.0f / (1.0f + linear * dist + quadratic * (dist * dist));
         
         
-        result += diffuse * attenuation + specular * attenuation;
+        dist = length(lights[i].lightPos - Vertex);
+        attenuation = 1.0f / (1.0f + linear * dist + quadratic * (dist * dist));
+        
+//        result += diffuse * attenuation + specular * attenuation;
     }
+    
+    DirectionalLightData data;
+    data.position = vec3(2.0f);
+    data.direction = vec3(0.0f, 0.0f, 0.0f) - data.position;
+    data.cutOff = cos(radians(12.5f));
+    
+    lightDir = normalize(data.position - Vertex);
+    
+    float theta = dot(lightDir, normalize(-data.direction));
+    
+    if(theta > data.cutOff) {
+        lightDir = normalize(data.position - Vertex);
+        diff = max(dot(normal, lightDir), 0.0f);
+        diffuse = diff * vec3(1.0f);
+        
+        reflectDir = reflect(-lightDir, normal);
+        
+        
+        specular = 0.5f * pow(max(dot(viewDir, reflectDir), 0.0f), 32) * vec3(1.0f, 0.0f, 0.0f);
+        
+        
+        dist = length(data.position - Vertex);
+        attenuation = 1.0f / (1.0f + linear * dist + quadratic * (dist * dist));
+        
+        result += diffuse + specular;
+    }
+    
     
     color = vec4(ambientLight + result, 1.0f) * texture(tex, UV);
     color.rgb = pow(color.rgb, vec3(2.2f));
 }
-
