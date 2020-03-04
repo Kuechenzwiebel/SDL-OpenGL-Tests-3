e1931e84e30dc503019f0a59e31e4f26f7b0b473
Tobias Pflüger <tobiaspflueger@t-online.de>
2020-03-04 17:55:54 +0100

    Added shader for phong lighting without specular component

---
+++ SDL-OpenGL-Tests-3/resources/shader/diffuse.fs
@@ -0,0 +1,106 @@
+#version 330 core
+
+out vec4 color;
+
+in vec3 Vertex;
+in vec2 UV;
+in vec3 Normal;
+
+
+uniform sampler2D tex;
+uniform vec3 viewPos;
+
+
+const float linear = 0.07f;
+const float quadratic = 0.017f;
+
+const vec3 ambientLight = vec3(0.1f);
+
+
+struct PointLightSourceData {
+    vec3 color;
+    vec3 position;
+};
+
+struct SpotLightSourceData {
+    vec3 position;
+    vec3 direction;
+    vec3 color;
+    float cutOff;
+    float outerCutOff;
+};
+
+uniform PointLightSourceData pointLights[300];
+uniform int validPointLights;
+
+uniform SpotLightSourceData spotLights[30];
+uniform int validSpotLights;
+
+uniform int reflection;
+uniform int useReflectionMap;
+uniform sampler2D reflectionMap;
+
+uniform int normalView;
+
+void main() {
+    vec3 result = vec3(0.0f);
+    
+    
+    vec3 forwardNormal = Normal;
+    vec3 backwardNormal = -forwardNormal;
+    vec3 lightDir;
+    float diffForward, diffBackward;
+    vec3 diffuse;
+    vec3 viewDir = normalize(viewPos - Vertex);
+    
+    float dist;
+    float attenuation;
+    
+    
+    int internalReflection;
+    if(useReflectionMap == 0) {
+        internalReflection = reflection;
+    }
+    else {
+        vec3 refMap = texture(reflectionMap, UV).rgb;
+        internalReflection = int((refMap.r + refMap.g + refMap.b) / 3.0f * 128);
+    }
+    
+    
+    
+    for(int i = 0; i < validPointLights; i++) {
+        dist = length(pointLights[i].position - Vertex);
+        
+        if(dist < 50.0f) {
+            lightDir = normalize(pointLights[i].position - Vertex);
+            diffForward = max(dot(forwardNormal, lightDir), 0.0f);
+            diffBackward = max(dot(backwardNormal, lightDir), 0.0f);
+            diffuse = (diffForward + diffBackward) * pointLights[i].color;
+            
+            attenuation = 1.0f / (1.0f + linear * dist + quadratic * (dist * dist));
+            
+            result += diffuse * attenuation;
+        }
+    }
+    
+    for(int i = 0; i < validSpotLights; i++) {
+        dist = length(spotLights[i].position - Vertex);
+        
+        if(dist < 50.0f) {
+            lightDir = normalize(spotLights[i].position - Vertex);
+            diffForward = max(dot(forwardNormal, lightDir), 0.0f);
+            diffBackward = max(dot(backwardNormal, lightDir), 0.0f);
+            diffuse = (diffForward + diffBackward) * spotLights[i].color;
+            
+            attenuation = 1.0f / (1.0f + linear * dist + quadratic * (dist * dist));
+            
+            result += diffuse * attenuation * clamp((dot(lightDir, normalize(-spotLights[i].direction)) - spotLights[i].outerCutOff) / (spotLights[i].cutOff - spotLights[i].outerCutOff), 0.0f, 1.0f);
+        }
+    }
+    
+    
+    if(normalView == 1)
+        color = vec4(forwardNormal, 1.0f);
+    else
+        color = vec4(ambientLight + result, 1.0f) * texture(tex, UV);
+}
---
+++ SDL-OpenGL-Tests-3/resources/shader/diffuse.vs
@@ -0,0 +1,20 @@
+#version 330 core
+
+layout (location = 0) in vec3 vertex;
+layout (location = 1) in vec2 uv;
+layout (location = 2) in vec3 normal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec3 Vertex;
+out vec2 UV;
+out vec3 Normal;
+
+void main() {
+    gl_Position = projection * view * model * vec4(vertex, 1.0f);
+    Vertex = vec3(model * vec4(vertex, 1.0f));
+    UV = vec2(uv.x, 1.0f - uv.y);
+    Normal = normalize(mat3(transpose(inverse(model))) * normal);  
+}
